ba342b950f3537fc6bc6eac84b623bd4614f47c9
ranauzairashraf <uzairashrafamazon05@gmail.com>
2022-12-18 16:32:06 +0500

    edit in all html and js function

--- Palindrome.js
+++ Palindrome.js
@@ -1,14 +1,20 @@
 function checkPalindrome() {
 
     var string = document.getElementById('pass').value;
-    
+    if(string.length > 10){
+        document.getElementById("demo").innerHTML = "Kindly Enter only 10 characters";
+    }else{
     const arrayValues = string.split('');
     const reverseArrayValues = arrayValues.reverse();
     const reverseString = reverseArrayValues.join('');
   if(string == reverseString) {
-      console.log('It is a palindrome');
+   
+      document.getElementById("demo").innerHTML = "It is a palindrome";
     }
   else {
-  console.log('It is not a palindrome');
+
+  document.getElementById("demo").innerHTML = "It is not a palindrome";
   }
 }
+}
+
